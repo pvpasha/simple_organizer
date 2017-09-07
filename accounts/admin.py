a93a88ae97a40d4c5e1682ba94f251d0a674f929
Oleksandr Klekha <klekhaav@gmail.com>
2017-09-07 21:43:47 +0300

    Custom user model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,14 +1,33 @@
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib import admin
 
+from .forms import UserChangeForm, UserCreationForm
 from .models import OrganizerUser
 
 
-class OrganizerUserAdmin(admin.ModelAdmin):
+class OrganizerUserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
     list_display = ('id', 'first_name', 'second_name', 'user_mail')
-    list_filter = ( 'first_name', 'second_name', 'user_mail')
+    list_filter = ('first_name', 'second_name', 'user_mail')
+
+    fieldsets = (
+        (None, {'fields': ('user_mail', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'second_name', 'is_staff',
+                                      'is_active', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {'fields': ('user_mail', 'password1', 'password2')}),
+        ('Extra info', {'classes': ('collapse',),
+                        'fields': ('first_name', 'second_name')}),
+    )
+
     search_fields = ('first_name', 'second_name', 'user_mail')
     empty_value_display = '-empty-'
     readonly_field = ['id',]
+
+    ordering = ('user_mail',)
 
 
 admin.site.register(OrganizerUser, OrganizerUserAdmin)
---
+++ accounts/forms.py
@@ -0,0 +1,40 @@
+from django import forms
+from django.contrib.auth.forms import (ReadOnlyPasswordHashField,
+                                       UserCreationForm as _UserCreationForm,
+                                       UserChangeForm as _UserChangeForm)
+
+from .models import OrganizerUser
+
+
+class UserCreationForm(_UserCreationForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = OrganizerUser
+        fields = ('user_mail', 'password1', 'password2', 'first_name', 'second_name')
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(_UserChangeForm):
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = OrganizerUser
+        fields = '__all__'
+
+    def clean_password(self):
+        return self.initial["password"]
