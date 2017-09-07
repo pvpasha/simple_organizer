cda4c4f92104d57752a6fa9b7579cff6336663d3
pvpasha <pv.pasha.pv@gmail.com>
2017-09-07 20:11:04 +0300

    Edit models in ToDo app

--- todo/models.py
+++ todo/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils import timezone, duration
+from datetime import date, datetime
 
 
 class ToDo(models.Model):
@@ -11,14 +11,15 @@ class ToDo(models.Model):
 
 class Task(models.Model):
     task_text = models.TextField(max_length=30)
-    time_for_work = models.DurationField
-    finished_time = models.DateTimeField
+    time_for_work = models.DurationField(default=0)
+    finished_time = models.DateTimeField(default=datetime.now)
+    alarm_clock_task = models.DateTimeField('Alarm clock:', default=datetime.now)
     check_task = models.BooleanField(default=False)
 
 
 class Contact(models.Model):
     name_c = models.CharField(max_length=20)
     forename_c = models.CharField(max_length=20)
-    phone_num = models.IntegerField
-    birthday_c = models.DateField
+    phone_num = models.IntegerField(default=0)
+    birthday_c = models.DateField(default=date.today)
 
