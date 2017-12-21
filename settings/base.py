5d11c924c3ef1656ef9fd7c59d6cb3082f5c7e95
pvpasha <pv.pasha.pv@gmail.com>
2017-12-21 22:16:05 +0200

    Reqirements add

--- settings/base.py
+++ settings/base.py
@@ -12,7 +12,7 @@ SECRET_KEY = 'key034950kjhkjhkjhu'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['myapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['sp-lutsk.com']
 
 # Application definition
 INSTALLED_APPS = [
