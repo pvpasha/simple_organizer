72491f6f1339d902268335ae214b8ea5da80eaf5
pvpasha <pv.pasha.pv@gmail.com>
2018-03-01 02:24:54 +0200

    Created and updated templates and js files

--- front/js/homeCtrl.js
+++ front/js/homeCtrl.js
@@ -8,6 +8,7 @@ angular
     function homeCtrl($scope){
         this.title = 'SP - Home';
         $scope.team = 'SP Lutsk';
+        $scope.brand = 'Simple Organizer'
 
         var date = new Date();
         $scope.today = date;
